a8234b3dea174412fc72e662ff6c45607054e804
Jo-Ann <princessgsm@live.nl>
2019-05-29 21:30:16 +0200

    Oops
    I may or may not have forgotten to keep my github up to date. 
    You can now shoot eggs with a chicken and have an egg spawn and grow if you hit the target

---
+++ BoltenJoAnn_BUILPWO2/Assets/Scripts/Player.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    
+    private Rigidbody animalRb;
+
+    public Transform hand;
+    public GameObject animal;
+    public Vector3 throwDirection;
+
+    public GameObject bulletEmitter;
+    public GameObject bullet;
+    public GameObject bulletMilk;
+    public float bulletForce;
+
+
+    public float damage = 10f;
+    public float range = 100f;
+	// Use this for initialization
+	void Start () {
+        //Zit er iets in mijn hand
+        if (hand.childCount > 0)
+        {
+            animal = hand.GetChild(0).gameObject;
+            
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (hand.childCount > 0)
+        {
+            animalRb = animal.GetComponent<Rigidbody>();
+            
+            if (Input.GetMouseButtonDown(1))
+            {
+                animal.transform.parent = null;
+                
+                animalRb.isKinematic = false;
+                animalRb.AddForce(throwDirection, ForceMode.Impulse);
+            }
+
+            else if (Input.GetMouseButtonDown(0))
+            {
+                GameObject tempBullet;
+                tempBullet = Instantiate(bullet, bulletEmitter.transform.position, bulletEmitter.transform.rotation) as GameObject;
+                RaycastHit hit;
+             
+                if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, range))
+                {
+
+             //       tempBullet.transform.rotation = hit.transform.rotation;
+                    /*Target target = hit.transform.GetComponent<Target>();
+                    if (target != null)
+                    {
+                        target.TakeDamage(damage);
+                    }*/
+                }
+                //Vector3 positionToLookat = transform.forward * 10;
+              
+
+                Rigidbody tempRb;
+                tempRb = tempBullet.GetComponent<Rigidbody>();
+
+                //Quaternion rot = Quaternion.LookRotation(positionToLookat, Vector3.up);
+
+                //tempBullet.transform.rotation = rot;
+
+                tempRb.AddForce(tempBullet.transform.forward * bulletForce);
+                Destroy(tempBullet, 10.0f);
+                
+            }
+        }
+
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit;
+
+                if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 2))
+                {
+                    if (hit.collider.CompareTag("Animal"))
+                    {
+                        hit.transform.SetParent(hand, false);
+                        hit.transform.localPosition = Vector3.zero;
+                       
+                        animal = hit.collider.gameObject;
+                        Debug.Log(animal);
+                    }
+                }
+            }
+
+            
+        }
+	}
+}
---
+++ BoltenJoAnn_BUILPWO2/Assets/Target.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+    public float health = 50f;
+    private int points = 0;
+    public GameObject ingredientPrefab;
+    public Transform pos;
+    private GameObject ingredient;
+    //public void TakeDamage(float amount)
+    //   {
+    //       health -= amount;
+    //       if (health <= 0)
+    //       {
+    //           Destroy(gameObject);
+    //       }
+    //   }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //When you hit the egg target, grow the main egg
+        if(collision.collider.tag == "Egg")
+        {
+            points += 1;
+            //check if ingredient is already instantiated otherwise dont make it
+            if (ingredient != null)
+            {
+                //increase it's size to get a giant egg
+                ingredient.transform.localScale += new Vector3(0.5F, 0.5F, 0.5F);
+            }
+            else
+            {
+                //create the egg ingredient and set it's gravity to false
+                ingredient = (GameObject)Instantiate(ingredientPrefab, pos);
+                ingredient.tag = "Egg Ingredient";
+                Rigidbody ingredientRb = ingredient.GetComponent<Rigidbody>();
+                ingredientRb.useGravity = false;
+            }         
+            
+            Debug.Log(points);
+        }
+    }
+
+    
+}
