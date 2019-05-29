76b30f797a2ac12450fdadd5b5d468feed8a2dbc
Jo-Ann <princessgsm@live.nl>
2019-05-29 22:48:38 +0200

    Reset and Drop Buttons
    + Additional fixes to shooting

---
+++ BoltenJoAnn_BUILPWO2/Assets/ButtonScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+
+    public GameObject ingredient;
+
+    private void OnMouseDown()
+    {
+        
+        if(this.CompareTag("ResetButton"))
+        {
+            //Check if object exists
+            if (GameObject.Find("Egg Ingredient") != null)
+            {
+                ingredient = GameObject.Find("Egg Ingredient");
+                Destroy(ingredient);
+            }
+        }
+
+        if (this.CompareTag("DropButton"))
+        {
+            //Check if object exists
+            if(GameObject.Find("Egg Ingredient") != null)
+            {
+                ingredient = GameObject.Find("Egg Ingredient");
+                Rigidbody ingredientRb = ingredient.GetComponent<Rigidbody>();
+                ingredientRb.useGravity = true;
+            }
+        }
+    }
+}
